0b79d65cab77694c8eea5496c0279a7c426d2db3
Peng Xiao <xiaoquwl@gmail.com>
2019-04-28 17:38:13 +0200

    django login view
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- account/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.Form):
-
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     # post views
-    path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
 ]
