d16af1db4b0096ab39fc34b102d3c26eac9956e0
Peng Xiao <xiaoquwl@gmail.com>
2019-04-30 07:19:22 +0200

    change password
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- account/urls.py
+++ account/urls.py
@@ -20,4 +20,18 @@ urlpatterns = [
     ),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path(
+        'password-change/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='account/password_change.html'
+        ),
+        name='password_change'
+    ),
+    path(
+        'password-change-done',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='account/password_change_done.html'
+        ),
+        name='password_change_done'
+    )
 ]
