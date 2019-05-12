5019bba46cfe622c1379477f783d21c134fda1ff
Peng Xiao <xiaoquwl@gmail.com>
2019-05-12 22:40:51 +0200

    add new model contact
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- account/models.py
+++ account/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contact(models.Model):
+
+    user_from = models.ForeignKey(
+        to=User,
+        related_name='from_set',
+        on_delete=models.CASCADE)
+    user_to = models.ForeignKey(
+        to=User,
+        related_name='to_set',
+        on_delete=models.CASCADE)
+    created = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{} follows {}'.format(
+            self.user_from, self.user_to)
+
+
+# Add following field to User dynamically
+User.add_to_class(
+    'following',
+    models.ManyToManyField(
+        'self',
+        through=Contact,
+        related_name='followers',
+        symmetrical=False)
+)
